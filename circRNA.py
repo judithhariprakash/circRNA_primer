e8026e214cb16d8b5876f9a25835fd87f3e7230f
judithhariprakash <judithharipraksh@gmail.com>
2018-02-13 14:55:06 +0100

    Update circRNA.py

--- circRNA.py
+++ circRNA.py
@@ -14,20 +14,20 @@ def get_input():
         strand = sys.argv[2].rstrip('\n')
         outfile = sys.argv[3].rstrip('\n')
     except IndexError:
-        print "USAGE:\npython circRNA.py coordinates strand outfile\n"
-        print "\t'coordinates': chrom:start-end format (exp: 10:25000000-25001000)"
-        print "\t'strand'     : can be '+' or '-'"
-        print "\t'outfile'    : Give a name where the output will be saved in CSV format"
+        print ("USAGE:\npython circRNA.py coordinates strand outfile\n")
+        print ("\t'coordinates': chrom:start-end format (exp: 10:25000000-25001000)")
+        print ("\t'strand'     : can be '+' or '-'")
+        print ("\t'outfile'    : Give a name where the output will be saved in CSV format")
         sys.exit()
     coord = coord.rstrip('\n').rstrip('\r')
     strand = strand.rstrip('\n').rstrip('\r')
     try:
         chrom, start, end = parse_coords(coord)
     except:
-        print "ERROR: Please provide coordinate in chrom:start-end format (exp: 10:25000000-25001000)"
+        print ("ERROR: Please provide coordinate in chrom:start-end format (exp: 10:25000000-25001000)")
         sys.exit()
     if strand not in ['+', '-']:
-        print "ERROR: Strand should be either '+' or '-'"
+        print ("ERROR: Strand should be either '+' or '-'")
         sys.exit()
     return chrom.upper().replace('CHR', ''), start, end, strand, outfile
 
@@ -35,8 +35,8 @@ def parse_coords(coord):
     chrom = coord.split(":")[0]
     start = coord.split(":")[1].split("-")[0]
     end = coord.split(":")[1].split("-")[1]
-    print "INFO: Input coordinates successfully parsed as following:"
-    print "INFO: Chrom:%s Start:%s End:%s" % (chrom, start, end)
+    print ("INFO: Input coordinates successfully parsed as following:")
+    print ("INFO: Chrom:%s Start:%s End:%s" % (chrom, start, end))
     return chrom, start, end
 
 def get_circ_coordinates(start, end, strand, offset=OFFSET):
@@ -54,46 +54,46 @@ def get_circ_coordinates(start, end, strand, offset=OFFSET):
         end2 = int(end)
     else:
         return False
-    print "INFO: Calculation of backspliced coordinates with offset of %dbp complete" % offset
+    print ("INFO: Calculation of backspliced coordinates with offset of %dbp complete" % offset)
     return ((start1, end1), (start2, end2))
 
 def fetch_ensembl(chrom, start, end, strand, species=SPECIES, assembly=ASSEMBLY):
-    print "INFO: Will connect to ENSEMBL for species: %s and assembly:%s" % (species, assembly)
+    print ("INFO: Will connect to ENSEMBL for species: %s and assembly:%s" % (species, assembly))
     server = "https://rest.ensembl.org"
     s = '1' if strand == '+' else '-1'
     ext = "/sequence/region/%s/%s:%s..%s:%s?coord_system_version=%s" % (
         species, chrom, str(start), str(end), s, assembly)
-    print "INFO:", server+ext
+    print ("INFO:", server+ext)
     r = requests.get(server+ext, headers={ "Content-Type" : "text/plain"})
     if not r.ok:
-        print "ERROR: Unable to contact Ensembl to fetch sequences. Exiting now.."
+        print ("ERROR: Unable to contact Ensembl to fetch sequences. Exiting now..")
         sys.exit()
-    print "INFO: Ensembl query successful"
+    print ("INFO: Ensembl query successful")
     return r.text
 
 def get_backspliced_seq(circ_cords):
-    print "INFO: Connecting to Ensembl for fetching 5' sequence"
+    print ("INFO: Connecting to Ensembl for fetching 5' sequence")
     seq1 = fetch_ensembl(chrom, circ_cords[0][0], circ_cords[0][1], strand)
-    print "INFO: Connecting to Ensembl for fetching 3' sequence"
+    print ("INFO: Connecting to Ensembl for fetching 3' sequence")
     seq2 = fetch_ensembl(chrom, circ_cords[1][0], circ_cords[1][1], strand)
     # Strand info is allow implicit in seq1 and seq2
     return seq1+seq2
 
 def run_primer3(seq):
-    print "INFO: Creating template file for Primer3 with template length of: %d" % len(seq)
+    print ("INFO: Creating template file for Primer3 with template length of: %d" % len(seq))
     temp_fn = str(uuid.uuid4())
     with open(temp_fn, 'w') as OUT:
         OUT.write("SEQUENCE_ID=sequence\nSEQUENCE_TEMPLATE=%s\n=\n" % seq)
     script_dir = os.path.dirname(os.path.realpath(__file__))
     exe = "%s/primer3_bin/primer3_core" % script_dir
     settings_fn = "%s/primer3_bin/circrna_primers_settings.p3" % script_dir
-    print "INFO: Using Primer3 settings file: %s" % settings_fn
+    print ("INFO: Using Primer3 settings file: %s" % settings_fn)
     cmd = "%s -p3_settings_file=%s -echo_settings_file %s" % (exe, settings_fn, temp_fn)
-    print "INFO: Running Primer3..."
-    print "INFO: ", cmd
+    print ("INFO: Running Primer3...")
+    print ("INFO: ", cmd)
     output = os.popen(cmd).read()
     os.system('rm %s' % temp_fn)
-    print "INFO: Removed temporary input file for Primer3"
+    print ("INFO: Removed temporary input file for Primer3")
     return output
 
 def prep_output(raw_output):
@@ -126,13 +126,13 @@ def prep_output(raw_output):
                     output_dict['PRIMER_LEFT_%d' % i].split(',')[1], output_dict['PRIMER_RIGHT_%d' % i].split(',')[1]
                 ]))
     else:
-        print "WARNING: No primers found"
+        print ("WARNING: No primers found")
         return "No primers found"
-    print "INFO: %d primers found" % valid_primers
+    print ("INFO: %d primers found" % valid_primers)
     return '\n'.join(parsed)
 
 if __name__ == '__main__':
-    print "Welcome to CircPrimer"    
+    print ("Welcome to CircPrimer")
     chrom, start, end, strand, outfile = get_input()
     cc = get_circ_coordinates(start, end, strand)
     back_seq = get_backspliced_seq(cc)
@@ -142,4 +142,4 @@ if __name__ == '__main__':
     primers_table = prep_output(primer3_output)
     with open("%s.csv" % outfile, 'w') as OUT:
         OUT.write(primers_table)
-    print "SUCCESS: Analysis complete.. Primers are saved in %s.csv" % outfile
+    print ("SUCCESS: Analysis complete.. Primers are saved in %s.csv" % outfile)
